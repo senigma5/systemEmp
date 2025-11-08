870ecaa5e8da0a8b4c03611d98c40c2671e3ea40
S ENIGMA <139472086+senigma5@users.noreply.github.com>
2025-11-09 00:03:16 +0300

    Add database connection script in test_db.php

---
+++ test_db.php
@@ -0,0 +1,15 @@
+<?php
+$servername = getenv("DB_HOST");
+$username = getenv("DB_USER");
+$password = getenv("DB_PASS");
+$database = getenv("DB_NAME");
+$port = getenv("DB_PORT");
+
+$conn = new mysqli($servername, $username, $password, $database, $port);
+
+if ($conn->connect_error) {
+    die("❌ Connection failed: " . $conn->connect_error);
+} else {
+    echo "✅ Successfully connected to the database!";
+}
+?>
