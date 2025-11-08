cec30fcbc12f11c48a3025472189e8b71f885e5f
S ENIGMA <139472086+senigma5@users.noreply.github.com>
2025-11-09 00:13:56 +0300

    Refactor database connection variables and error handling

--- db.php
+++ db.php
@@ -1,12 +1,15 @@
 <?php
-$host = getenv('MYSQLHOST');
-$user = getenv('MYSQLUSER');
-$pass = getenv('MYSQLPASSWORD');
-$db   = getenv('MYSQLDATABASE');
-$port = getenv('MYSQLPORT');
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$servername = getenv("MYSQLHOST");
+$username   = getenv("MYSQLUSER");
+$password   = getenv("MYSQLPASSWORD");
+$database   = getenv("MYSQLDATABASE");
+$port       = getenv("MYSQLPORT");
 
 try {
-    $conn = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $pass);
+    $conn = new PDO("mysql:host=$servername;port=$port;dbname=$database", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "✅ Successfully connected to the database!";
 } catch (PDOException $e) {
