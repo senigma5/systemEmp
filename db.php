ee9b63d63e7c2f604162c31c431f11c94e938d5a
S ENIGMA <139472086+senigma5@users.noreply.github.com>
2025-11-09 22:02:07 +0300

    Fix mysqli instantiation syntax error

--- db.php
+++ db.php
@@ -5,7 +5,7 @@ $pass = getenv('MYSQLPASSWORD');
 $dbname = getenv('MYSQLDATABASE');
 $port = getenv('MYSQLPORT');
 
-$conn = nw mysqli($host, $user, $pass, $dbname, $port);
+$conn = new mysqli($host, $user, $pass, $dbname, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
